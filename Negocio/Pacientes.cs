027c15a75b84a4f3ffbb7618e3175794f899e05a
Joel <joelreynoso51@gmail.com>
2022-06-17 17:00:45 -0400

    Metodo Crear Paciente #1

--- Negocio/Pacientes.cs
+++ Negocio/Pacientes.cs
@@ -17,5 +17,14 @@ namespace Negocio
             Tabla = ObjetoPaciente.Mostrar();
             return Tabla;
         }
+
+        public void CrearPaciente(string NExpediente, string Nombre, string Apellido, string Direccion, string Telefono, string Celular, string FechaNaciemiento, int Edad, string TipoSangre, int Documento, string NumeroDocumento, int Sexo, int EstadoCivil, string LugarNacimiento, string Email, string Ocupacion, int ParentescoFamiliar1, string NombreFamiliar1, string TelefonoFamiliar1, string DireccionFamiliar1, int ParentescoFamiliar2, string NombreFamiliar2, string TelefonoFamiliar2, string DireccionFamiliar2)
+        {
+            ObjetoPaciente.CrearExpediente(NExpediente,Nombre,Apellido,
+                Direccion,Telefono,Celular,FechaNaciemiento,Edad,TipoSangre,Documento,
+                NumeroDocumento,Sexo,EstadoCivil,LugarNacimiento,Email,Ocupacion,ParentescoFamiliar1,
+                NombreFamiliar1,TelefonoFamiliar1,DireccionFamiliar1,ParentescoFamiliar2,
+                NombreFamiliar2,TelefonoFamiliar2,DireccionFamiliar2);
+        }
     }
 }
